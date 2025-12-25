ae2827bf2b09f4eac649ee8e2fdb06e83bab9ecd
Naifi <247718176+rufnx@users.noreply.github.com>
2025-12-26 00:06:39 +0700

    build_kernel: rework #4

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
 N="\e[0m"
@@ -8,18 +7,18 @@ G="\e[1;32m"
 Y="\e[1;33m"
 B="\e[1;34m"
 
-_log() {
-  echo -e "${2}${1}${N} ${3}"
-}
-
+_log() { echo -e ${2}${1}${N} ${3}; }
 info() { _log "[•]" "$B" "$1"; }
 ok()   { _log "[√]" "$G" "$1"; }
 warn() { _log "[!]" "$Y" "$1"; }
 err()  { _log "[x]" "$R" "$1"; exit 1; }
 
 fetch() {
-  curl -LSs https://raw.githubusercontent.com/rufnx/personal_patch/master/clone.sh \
-    | bash -s "$1" "$2"
+  curl -LSs https://raw.githubusercontent.com/rufnx/personal_patch/master/clone.sh | bash -s $1 $2
+}
+
+fetch_anykernel() {
+  curl -LSs https://raw.githubusercontent.com/rufnx/personal_patch/master/ak3.sh | bash -s $1 $2
 }
 
 toolchains() {
@@ -33,34 +32,33 @@ toolchains() {
 }
 
 setup_env() {
-
-  export PATH="$PWD/clang/bin:$PWD/gcc/bin:$PATH"
+  export PATH=$(pwd)/clang/bin:$(pwd)/gcc/bin:$PATH
   export USE_CCACHE=1
-  export KCFLAGS="-w"
-  export KBUILD_BUILD_HOST="$(hostname)"
-  export KBUILD_BUILD_USER="$(whoami)"
+  export KCFLAGS=-w
+  export KBUILD_BUILD_HOST=$(hostname)
+  export KBUILD_BUILD_USER=$(whoami)
 }
 
 compile() {
   toolchains
   setup_env
 
-  local cfg_dir="arch/arm64/configs"
-  local defconfig
+  local DF_DIR=arch/arm64/configs
+  local DEFCONFIG
 
-  if [[ -f "$cfg_dir/a22x_defconfig" ]]; then
-    defconfig="a22x_defconfig"
-  elif [[ -f "$cfg_dir/vendor/a22x_defconfig" ]]; then
-    defconfig="vendor/a22x_defconfig"
+  if [[ -f $DF_DIR/a22x_defconfig ]]; then
+    DEFCONFIG=a22x_defconfig
+  elif [[ -f $DF_DIR/vendor/a22x_defconfig ]]; then
+    DEFCONFIG=vendor/a22x_defconfig
   else
     err "Defconfig not found"
   fi
 
-  info "Generating defconfig: $defconfig"
-  make ARCH=arm64 O=out "$defconfig"
+  info "Generating defconfig: $DEFCONFIG"
+  make ARCH=arm64 O=out $DEFCONFIG
 
   info "Building kernel"
-  make -j"$(nproc)" \
+  make -j$(nproc) \
     O=out \
     ARCH=arm64 \
     LLVM=1 \
@@ -74,22 +72,43 @@ build() {
   info "Start build"
   compile 2>&1 | tee build.log
 
-  local obj="out/arch/arm64/boot"
-  local output
+  local OBJ=out/arch/arm64/boot
+  local BRANCH=a22x
+  local ZIPNAME=AnyKernel3-A226.zip
+  local OUTPUT VERSION
 
-  if [[ -f "$obj/Image.gz" ]]; then
-    output="$obj/Image.gz"
-  elif [[ -f "$obj/Image" ]]; then
-    output="$obj/Image"
+  if [[ -f $OBJ/Image.gz ]]; then
+    OUTPUT=$OBJ/Image.gz
+    VERSION=$(zcat $OUTPUT | strings | grep "Linux version" || true)
+  elif [[ -f $OBJ/Image ]]; then
+    OUTPUT=$OBJ/Image
+    VERSION=$(strings $OUTPUT | grep "Linux version" || true)
   else
     grep -i error build.log || true
     err "Kernel output not found"
   fi
 
   mkdir -p result
-  cp "$output" result/
+  cp $OUTPUT result/
+  cd result
 
-  ok "Kernel build success → result/$(basename "$output")"
+  ok "Kernel build success"
+
+  [[ -f $ZIPNAME ]] && rm -f $ZIPNAME
+
+  if [[ ! -d ak ]]; then
+    fetch_anykernel $BRANCH ak
+  else 
+    rm -rf ak/Image ak/Image.gz
+  fi
+
+  cp $OUTPUT ak/
+  cd ak
+  zip -r9 ../$ZIPNAME *
+  cd ..
+
+  ok "Build finish: $ZIPNAME"
+  [[ -n $VERSION ]] && ok "Version: $VERSION"
 }
 
-build "$@"
+build $@
